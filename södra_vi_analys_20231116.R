8eb7343b74eb2225b1160e996d7c4825cf10aa36
Liziniewicz Mateusz <mateusz.liziniewicz@skogforsk.se>
2023-12-07 15:27:03 +0100

    changes in structure

--- södra_vi_analys_20231116.R
+++ södra_vi_analys_20231116.R
@@ -1,21 +1,48 @@
 setwd("C:/Users/mali/OneDrive - Skogforsk/BJÖRK/Björk_gallrig")
 
-exp = "F1248"
+exp = "S1325"
+
+year <- read_excel("DB_S1325_SödraVi.xlsx", sheet = "Försökmeta", na = ".") %>%
+  filter(Exp == exp)
+
+year <- year$Planteringsar
 
 
 library(readxl)
 library(tidyverse)
-stems <- read_excel("DB_S1325_SödraVi.xlsx", sheet = "Data", na = ".") %>%
-  filter(Exp == exp)
-df <- stems
+df <- read_excel("DB_S1325_SödraVi.xlsx", sheet = "Data", na = ".") %>%
+  filter(Exp == exp) %>% mutate(AGE = AR - year + 1)
 
+
+
+#####judge species######################################
+get_most_common_value_numeric <- function(data_frame, variable_name, result_variable_name) {
+  # Extract the specified variable
+  selected_variable <- data_frame[[variable_name]]
+  
+  # Use table() to get frequency of each unique value
+  frequency_table <- table(selected_variable)
+  
+  # Find the most common value and convert to numeric
+  most_common_value_numeric <- as.numeric(names(frequency_table)[which.max(frequency_table)])
+  
+  # Assign the result to a variable in the global environment
+  assign(result_variable_name, most_common_value_numeric, envir = .GlobalEnv)
+  return(cat("Dominant species is: ", most_common_value_numeric))
+  
+}
+
+# Example usage with a data frame named 'your_data_frame' and a variable named 'your_variable'
+
+get_most_common_value_numeric(df, "TRSL", "species")
+#########################################################################################
 
 
 
 gal <- read_excel("DB_S1325_SödraVi.xlsx", sheet = "Försök", na = ".") %>%
   filter(EXP == exp) %>%
-  select(YTA, AR, UTF)  
-gal
+  select(YTA, AR, UTF) %>% mutate(AGE = AR - year + 1) %>% select(-AR)
+
 beh <- read_excel("DB_S1325_SödraVi.xlsx", sheet = "Försök", na = ".") %>% 
   filter(EXP == exp) %>%
   select(YTA, BEH) %>%
@@ -37,26 +64,30 @@ df_total <- fit_simple_model(df)
 
 
 
-df1 <- left_join(df, df_total, by = c("AR"))
+df1 <- left_join(df, df_total, by = c("AGE"))
 df2 <- left_join(df1, beh, by = c("YTA")) %>%
   mutate(hest = a*D^b) %>% select(-a, -b)
 
 
 
 
-birch_volume <- function(D, H, na.rm = FALSE){
+vol_calc <- function(D, H, TRSL = 10, na.rm = FALSE) {
   
   if (na.rm) {
     D <- na.omit(D)
     H <- na.omit(H)
   }
   
-  V <- (10^(-0.89363) * D^2.23818 * (D + 20.0)^(-1.06930) * H^6.02015 * (H - 1.3)^(-4.51472))/1000
-  return(V)
+  vol <- ifelse(TRSL == 30,
+                (10^(-0.89363) * D^2.23818 * (D + 20.0)^(-1.06930) * H^6.02015 * (H - 1.3)^(-4.51472))/1000,
+                ifelse(TRSL == 20,
+                       10^(-1.02039) * D^2.00128 * (D + 20)^(-0.47473) * H^2.87128 * (H - 1.3)^(-1.61083)/1000,
+                       NA))
+  
+  return(vol)
 }
 
-
-basalA <- function(D, na.rm = FALSE){
+basalA <- function(D, na.rm = FALSE){ #basal area function
   if(na.rm) {
     D <- na.omit(D)
   }
@@ -64,32 +95,31 @@ basalA <- function(D, na.rm = FALSE){
   BA <- pi*(D/2000)^2
 }
 
-df2 <- df2 %>% mutate(vol = birch_volume(D/10, hest/10), ba = basalA(D))
+df2 <- df2 %>% mutate(vol = vol_calc(D/10, hest/10, TRSL = TRSL), ba = basalA(D))
+
 
 df2 <- left_join(df2, area, by = "YTA")
-
-tail(df2)
 
 
 ####volume calculations
 
 
-kvar <- df2 %>% group_by(YTA, BEH,  AR) %>% filter(!is.na(D) | MORT3 == 1) %>%
+kvar <- df2 %>% group_by(YTA, BEH,  AGE) %>% filter(!is.na(D) | MORT3 == 1) %>%
   summarise(sumvol = sum(vol, na.rm = T)*mean(AREA), sumba = sum(ba, na.rm = T)*mean(AREA)) 
 
 
 
-utgall <- df2 %>% group_by(YTA, BEH, AR) %>% filter(MORT3 == 0) %>%
+utgall <- df2 %>% group_by(YTA, BEH, AGE) %>% filter(MORT3 == 0) %>%
   summarise(sumvolGall = sum(vol, na.rm = T)*mean(AREA), sumbaGall = sum(ba, na.rm = T)*mean(AREA))
 
 
-dod <- df2 %>% filter(MORT3 == 5) %>% group_by(YTA, BEH, AR)  %>% 
+dod <- df2 %>% filter(MORT3 == 5) %>% group_by(YTA, BEH, AGE)  %>% 
   summarise(sumvolDod = sum(vol, na.rm = T)*mean(AREA), sumbaDod = sum(ba, na.rm = T)*mean(AREA))
 
 
 
 
-volume <- left_join(kvar, utgall, by = c("YTA", "BEH", "AR")) %>% ungroup() %>%
+volume <- left_join(kvar, utgall, by = c("YTA", "BEH", "AGE")) %>% ungroup() %>%
   mutate(sumvolGall  = ifelse(is.na(sumvolGall ), 0, sumvolGall )) %>%
   mutate(kvarvol = sumvol - sumvolGall) %>% group_by(YTA, BEH) %>%
   mutate(utgall = cumsum(sumvolGall)) %>%
@@ -100,7 +130,7 @@ volume[is.na(volume)] <- 0
 
 
 #to result table##########################
-res_volume <- volume %>% select(YTA, BEH, AR, sumvol, sumvolGall, kvarvol, utgall, totvol) %>%
+res_volume <- volume %>% select(YTA, BEH, AGE, sumvol, sumvolGall, kvarvol, utgall, totvol) %>%
   rename(volfg = sumvol, volut = sumvolGall, voleg = kvarvol, volut_cum = utgall, Totvol = totvol)
 
 ##############volym figure###########################
@@ -108,18 +138,18 @@ volume
 volume <- volume[rep(seq_len(nrow(volume)), each = 2), ]
 volume[is.na(volume)] <- 0
 volume
-for (i in seq(1, length(volume$AR), 2)){
+for (i in seq(1, length(volume$AGE), 2)){
   volume$kvarvol[i] = volume$kvarvol[i] + volume$sumvolGall[i]
 }
 volume
-ggplot(aes(x = AR, y = kvarvol), data = volume) + geom_line() + facet_wrap(YTA~BEH )
+ggplot(aes(x = AGE, y = kvarvol), data = volume) + geom_line() + facet_wrap(YTA~BEH )
 
 
 
 #####################################################################################
 
 #############basal area##############################
-basalarea <- left_join(kvar %>% select(-sumvol), utgall %>% select(-sumvolGall), by = c("YTA", "BEH", "AR")) %>% ungroup() %>%
+basalarea <- left_join(kvar %>% select(-sumvol), utgall %>% select(-sumvolGall), by = c("YTA", "BEH", "AGE")) %>% ungroup() %>%
   mutate(sumbaGall  = ifelse(is.na(sumbaGall ), 0, sumbaGall )) %>%
   mutate(kvarba = sumba - sumbaGall) %>% group_by(YTA, BEH) %>%
   mutate(utgallba = cumsum(sumbaGall)) %>%
@@ -129,7 +159,7 @@ basalarea <- left_join(kvar %>% select(-sumvol), utgall %>% select(-sumvolGall),
 basalarea[is.na(basalarea)] <- 0
 
 
-res_ba <- basalarea %>% select(YTA, BEH, AR, sumba, sumbaGall, kvarba, utgallba, totba) %>%
+res_ba <- basalarea %>% select(YTA, BEH, AGE, sumba, sumbaGall, kvarba, utgallba, totba) %>%
   rename(bafg = sumba, baut = sumbaGall, baeg = kvarba, baut_cum = utgallba, Totba = totba)
 
 basalarea <- basalarea[rep(seq_len(nrow(basalarea)), each = 2), ]
@@ -137,19 +167,19 @@ basalarea <- basalarea[rep(seq_len(nrow(basalarea)), each = 2), ]
 
 
 ##BA figure##################
-for (i in seq(1, length(basalarea$AR), 2)){
+for (i in seq(1, length(basalarea$AGE), 2)){
   basalarea$kvarba[i] = basalarea$kvarba[i] + basalarea$sumbaGall [i]
 }
 basalarea
-ggplot(aes(x = AR, y = kvarba), data = basalarea) + geom_line() + facet_wrap(YTA~BEH )
+ggplot(aes(x = AGE, y = kvarba), data = basalarea) + geom_line() + facet_wrap(YTA~BEH )
 
-ggplot(aes(x = AR, y = kvarba, group = YTA, color = factor(BEH)), data = basalarea) + geom_line() + geom_point() + facet_wrap(~BEH )
+ggplot(aes(x = AGE, y = kvarba, group = YTA, color = factor(BEH)), data = basalarea) + geom_line() + geom_point() + facet_wrap(~BEH )
 
 
 
 
 ##############MAI################
-ggplot(aes(x = AR-1995+2, y = totvol/(AR-1995+2)), data = volume) + 
+ggplot(aes(x = AGE, y = totvol/(AGE)), data = volume) + 
   geom_line(aes(group = YTA, color = BEH), linewidth = 1) +
   xlab("Ålder") + ylab("MAI (m3 ha år)") +
   theme_bw()
@@ -158,7 +188,7 @@ ggplot(aes(x = AR-1995+2, y = totvol/(AR-1995+2)), data = volume) +
 
 
 ########## Antal ################
-antal1 <- df2 %>% filter(!is.na(MORT3)) %>% group_by(YTA, BEH, AR, MORT3) %>%
+antal1 <- df2 %>% filter(!is.na(MORT3)) %>% group_by(YTA, BEH, AGE, MORT3) %>%
   summarise(li = round(sum(!is.na(MORT3)) * mean(AREA, na.rm = T), 0))
 
 
@@ -169,22 +199,29 @@ library(reshape2)
 library(dplyr)
 
 process_antal_data <- function(antal1) {
-  antal1 <- reshape2::dcast(antal1, YTA + BEH + AR ~ MORT3, value.var = "li")
+  antal1 <- reshape2::dcast(antal1, YTA + BEH + AGE ~ MORT3, value.var = "li")
   
-  names(antal1)[names(antal1)== "0"] <- "utgall_n" #removed in thinning
-  names(antal1)[names(antal1)=="1"] <- "n" # left in stand
-  names(antal1)[names(antal1)== "5"] <- "dod_n" # natural mortality
+  names(antal1)[names(antal1) == "0"] <- "utgall_n" # removed in thinning
+  names(antal1)[names(antal1) == "1"] <- "n"       # left in stand
+  names(antal1)[names(antal1) == "5"] <- "dod_n"   # natural mortality
+  
   if (!all(c("utgall_n", "n", "dod_n") %in% names(antal1))) {
-    antal1 <- antal1 %>% mutate(dod_n = 0)
+    if (!"utgall_n" %in% names(antal1)) {
+      antal1 <- antal1 %>% mutate(utgall_n = 0)
+    }
+    if (!"dod_n" %in% names(antal1)) {
+      antal1 <- antal1 %>% mutate(dod_n = 0)
+    }
   }
+  
   antal1[is.na(antal1)] <- 0
+  
   res_antal <- antal1 %>% group_by(YTA, BEH) %>%
-     mutate(nfg = n + utgall_n + dod_n, nut = utgall_n, neg = n) %>%
+    mutate(nfg = n + utgall_n + dod_n, nut = utgall_n, neg = n) %>%
     select(-utgall_n, -n) %>%
     mutate(nutcum = cumsum(nut))
   
   return(res_antal)
-  
 }
 
 # Example usage:
@@ -199,11 +236,11 @@ antal1[is.na(antal1)] <- 0
 
 
 
-for (i in seq(1, length(antal1$AR), 2)) {
+for (i in seq(1, length(antal1$AGE), 2)) {
   antal1$neg[i] = antal1$neg[i] + antal1$nut[i] + antal1$dod_n[i] }
 
 
-ggplot(aes(x = AR, y = neg), data = antal1) + geom_line() + facet_wrap(YTA~BEH )
+ggplot(aes(x = AGE, y = neg), data = antal1) + geom_line() + facet_wrap(YTA~BEH )
 
 
 #################################
@@ -213,39 +250,39 @@ ggplot(aes(x = AR, y = neg), data = antal1) + geom_line() + facet_wrap(YTA~BEH )
 ### results diameter_height##############################
 ##########################################################
 #before thinning
-res_height_diameter_fg <- df2 %>% group_by(YTA, BEH, AR) %>%
+res_height_diameter_fg <- df2 %>% group_by(YTA, BEH, AGE) %>%
   summarise(mh_fg = mean(hest, na.rm = T), md_fg = mean(D, na.rm = T)) 
 
 
 #thinned = 0 and unthinnded = 1
-res_height_diameter<- df2 %>% group_by(YTA, BEH, AR, MORT3) %>%
+res_height_diameter<- df2 %>% group_by(YTA, BEH, AGE, MORT3) %>%
   summarise(mh = round(mean(hest, na.rm = T), 0), md = round(mean(D, na.rm = T), 0)) %>%
   filter(!is.na(MORT3))
 
 #height
-hh <- reshape2::dcast(res_height_diameter, YTA + BEH + AR ~ MORT3, value.var = "mh")
+hh <- reshape2::dcast(res_height_diameter, YTA + BEH + AGE ~ MORT3, value.var = "mh")
 
 names(hh)[names(hh)== "0"] <- "mh_ut" #removed in thinning
 names(hh)[names(hh)=="1"] <- "mh_eg" # left in stand'
 hh[is.na(hh)] <- "0"
 hh
 #diameter
-dd <- reshape2::dcast(res_height_diameter, YTA + BEH + AR ~ MORT3, value.var = "md")
+dd <- reshape2::dcast(res_height_diameter, YTA + BEH + AGE ~ MORT3, value.var = "md")
 names(dd)[names(dd)== "0"] <- "md_ut" #removed in thinning
 names(dd)[names(dd)=="1"] <- "md_eg" # left in stand'
 
 dd[is.na(dd)] <- "0"
 
-hd <- left_join(res_height_diameter_fg, hh, by = c("YTA", "BEH", "AR"))
-hd <- left_join(hd, dd, by = c("YTA", "BEH", "AR")) %>% 
-  select("YTA", "BEH", "AR", "mh_fg", "mh_ut", "mh_eg", "md_fg", "md_ut", "md_eg")
+hd <- left_join(res_height_diameter_fg, hh, by = c("YTA", "BEH", "AGE"))
+hd <- left_join(hd, dd, by = c("YTA", "BEH", "AGE")) %>% 
+  select("YTA", "BEH", "AGE", "mh_fg", "mh_ut", "mh_eg", "md_fg", "md_ut", "md_eg")
 hd
 
 rm(pri)
-pri <- left_join(res_antal, res_volume, by = c("YTA", "BEH", "AR"))
-pri <- left_join(pri, hd, by = c("YTA", "BEH", "AR"))
-pri <- left_join(pri, res_ba, by = c("YTA", "BEH", "AR")) %>% mutate(GALL = ifelse(volut > 1, 1, 0)) %>%
-  select(YTA, BEH, AR, GALL, everything())
+pri <- left_join(res_antal, res_volume, by = c("YTA", "BEH", "AGE"))
+pri <- left_join(pri, hd, by = c("YTA", "BEH", "AGE"))
+pri <- left_join(pri, res_ba, by = c("YTA", "BEH", "AGE")) %>% mutate(GALL = ifelse(volut > 1, 1, 0)) %>%
+  select(YTA, BEH, AGE, GALL, everything()) %>% mutate(MAI = Totvol/AGE)
 
-
+pri
 write.csv(pri %>% mutate_if(is.numeric, list(~format(., nsmall = 1))), paste(exp, "_results.csv", sep = ""), row.names = F, fileEncoding = "UTF-8" )
